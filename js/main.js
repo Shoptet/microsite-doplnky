3e2fd147dfaf8b07adce9da2da6ad9ac1849e3a4
Radek Prochazka <prochazka@shoptet.cz>
2018-06-08 16:54:40 +0200

    Add naja support

--- js/main.js
+++ js/main.js
@@ -4,12 +4,90 @@ $(function() {
         maxWidth: '98%'
     });
 
-    $('html').on('change', '#filter-form select', function() {
-        // TODO: overlay
-        $('body').prepend('<div class="overlay"><div class="loader"></div></div>').addClass('overlay-visible');
-        $(this).parents('form').submit();
-    });
 });
+
+$(window).bind("load", function() {
+
+    // filter form submit without click to submit button
+    var sels = document.querySelectorAll('#frm-filterForm select'), i;
+    for (i = 0; i < sels.length; ++i) {
+        sels[i].addEventListener('change',function() {
+            loader = new Loader('body', true);
+            loader.show();
+            document.getElementById('frm-filterForm').submit();
+        }, false);
+    }
+
+});
+
+
+
+/**
+ * Class Loader, show or hide loader
+ *
+ * @param idElement
+ * @param forceRelative
+ *
+*/
+function Loader(idParent, forceRelative) {
+    this.idParent = idParent;
+    this.id = this.idParent + '-loader-' + (((1+Math.random())*0x10000)|0).toString(16).substring(1);
+    this.wrapper = document.getElementById(this.idParent);
+    if (!this.wrapper) {
+        throw('Loader: Element with id' + this.idParent + ' not found.');
+    }
+    style = window.getComputedStyle(this.wrapper);
+    if (style.getPropertyValue('position') != 'relative' && forceRelative) {
+        this.wrapper.style.position = "relative";
+    }
+    if (style.getPropertyValue('position') != 'relative' && !forceRelative) {
+        throw('Loader: Element must set up css position:relative');
+    }
+    this.overlay;
+
+}
+Loader.prototype.show = function() {
+    overlay = document.getElementById(this.id);
+    if (!overlay) {
+        var loader = document.createElement('div');
+        loader.classList.add('loader');
+        this.overlay = document.createElement('div');
+        this.overlay.setAttribute('id', this.id);
+        this.overlay.classList.add('loader-overlay');
+        this.overlay.appendChild(loader);
+        if (this.idParent == 'body') {
+            this.overlay.style.height = '100vh';
+        }
+        this.wrapper.appendChild(this.overlay);
+    } else {
+        this.overlay.style.display = 'block';
+    }
+}
+Loader.prototype.hide = function() {
+    this.overlay.style.display = 'none';
+}
+
+
+/**
+ * @param naja
+ * @param idElement
+ * @param forceRelative
+ */
+function LoaderExtension(naja, idElement, forceRelative) {
+    naja.addEventListener('init', function () {
+        this.loader = new Loader(idElement, forceRelative);
+    }.bind(this));
+    naja.addEventListener('start', showLoader.bind(this));
+    //don`t call hide, because content of div is replaced by ajax response
+    //naja.addEventListener('complete', hideLoader.bind(this));
+
+    function showLoader() {
+        this.loader.show();
+    }
+    return this;
+}
+
+
 
 if (typeof Nette !== 'undefined') {
 
@@ -24,7 +102,7 @@ if (typeof Nette !== 'undefined') {
             element.focus();
         }
         if (message) {
-            var e = document.createElement('span');
+            var e = document.createElement('div');
             e.innerText = message;
             e.className = Nette.formErrorClass;
             element.parentNode.insertBefore(e, element.nextSibling);
